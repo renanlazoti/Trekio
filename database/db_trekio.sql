f3d7717ba6bff5bfa5bee43be8fe8f9784186e99
fernando <mirandafernando159@yahoo.com>
2024-05-02 19:40:15 -0300

    feat(back): cria a tabela de logs

---
+++ database/db_trekio.sql
@@ -0,0 +1,10 @@
+create database db_trekio;
+use db_trekio;
+
+create table tb_logs(
+	id int primary key auto_increment,
+    perguntaChatGPT varchar(255) not null,
+    respostaChatGPT varchar(255) not null
+);
+
+select * from tb_logs;
