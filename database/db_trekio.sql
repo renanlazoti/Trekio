42e096ecda28331f5e2d2bc68b11914110d56f3b
Andre Sena <andresena07092004@gmail.com>
2024-05-15 21:18:18 -0300

    feat(back): refatorando banco de dados

--- database/db_trekio.sql
+++ database/db_trekio.sql
@@ -20,3 +20,19 @@ create table tb_logs(
 );
 
 select * from tb_logs;
+
+
+# ATUALIZAÇÃO FINAL BD_Trekio
+
+create database db_trekio;
+
+use db_trekio;
+
+create table tb_logs(
+	id int primary key auto_increment,
+	perguntaChatGPT varchar(255),
+    respostaChatGPT varchar(255),
+    logsDeConsultas varchar(255) not null
+);
+
+select * from tb_logs;
