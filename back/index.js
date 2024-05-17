9dd066ba744a5e110e265f92d57c94e843f5a192
fernando <mirandafernando159@yahoo.com>
2024-05-16 22:30:57 -0300

    feat(back): cria comunicação com o ChatGPT

--- back/index.js
+++ back/index.js
@@ -2,8 +2,35 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const mysql = require('mysql');
+const { OpenAI } = require('openai');
 const moment = require('moment');
 require("dotenv").config();
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const openai = new OpenAI(OPENAI_API_KEY);
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.post('/pergunte-ao-chatgpt', async (req, res) => {
+    const { prompt } = req.body;
+
+    const model = 'gpt-3.5-turbo';
+    const role = 'user';
+    const max_tokens = 50;
+
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: role, content: prompt}],
+        model: model,
+        max_tokens: max_tokens
+    });
+
+    res.json({completion: completion.choices[0].message.content});
+});
 
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env
 const pool = mysql.createPool({
@@ -15,14 +42,6 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0,
 });
-
-app.get('/hello-world', (req, res) => {
-    res.send('Hello World')
-});
-
-app.listen(3000, () =>  
-    console.log('Serviço em execução na porta 3000')
-);
 
 app.use((req, res, next) => {
     const currentTime = moment().format();
