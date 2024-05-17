f30ee224619e4588ea93f5d98a2e56e945407c8b
renan <devrenanlaz@gmail.com>
2024-05-16 21:02:09 -0300

    feat(front): cria componente travel guide

--- front/src/components/main/Main.js
+++ front/src/components/main/Main.js
@@ -1,33 +1,13 @@
 import React from 'react'
 import './main.css'
-import { FaPlaneDeparture } from "react-icons/fa";
-import { Link } from 'react-router-dom';
-import svgAirPlane from '../../assets/img/fi-rr-plane.svg'
-import { IoLocationSharp } from "react-icons/io5";
+import { TravelGuide } from '../travelGuide/TravelGuide'
 
 export const Main = () => {
   return (
     <section className='content'>
         <div className="content-introduction">
           <p className='app__main-introduction-text'><strong>Trilhe</strong><br />suas próximas aventuras <br />com nossos roteiros!</p>
-          <div className='content-howtotravel'>
-            <div className='content-howtotravel-item'>
-              <IoLocationSharp className='content-howtotravel-img' /> 
-              <p>Informe seu destino</p>
-            </div>
-            &gt;
-            <div className='content-howtotravel-item'>
-              <img src={svgAirPlane} alt="" className='content-howtotravel-img' />
-              <p>Tempo da sua jornada</p>
-            </div>
-            &gt;
-            <div className='content-howtotravel-item'>
-              <img src={svgAirPlane} alt="" className='content-howtotravel-img' />
-              <p>Seu objetivo</p>
-            </div>
-            &gt;
-            <Link to="/chat" className='app__main-introduction-button'><FaPlaneDeparture/>Descubra</Link>
-          </div>
+          <TravelGuide />
         </div>
     </section>
   )
---
+++ front/src/components/travelGuide/TravelGuide.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './travelGuide.css'
+
+import { FaPlaneDeparture } from "react-icons/fa";
+import { IoLocationOutline } from "react-icons/io5";
+import { BsCalendarDate } from "react-icons/bs";
+import { PiClipboardText } from "react-icons/pi";
+
+export const TravelGuide = () => {
+  return (
+    <div className='travelGuide__content'>
+        <div className='travelGuide__item'>
+            <IoLocationOutline className='travelGuide__item-img' /> 
+            <div className='travelGuide__item-information'>
+              <p>Informe seu</p>
+              <p><strong>Destino</strong></p>
+            </div>
+        </div>
+        &gt;
+        <div className='travelGuide__item'>
+          <BsCalendarDate className='travelGuide__item-img' />
+          <div className='travelGuide__item-information'>
+            <p>Data da sua</p>
+            <p><strong>Jornada</strong></p>
+          </div>
+        </div>
+        &gt;
+        <div className='travelGuide__item'>
+          <PiClipboardText  className='travelGuide__item-img' /> 
+          <div className='travelGuide__item-information'>
+            <p>Escolha o seu</p>
+            <p><strong>Objetivo</strong></p>
+          </div>
+        </div>
+        &gt;
+        <Link to="/chat" className='app__main-introduction-button'><FaPlaneDeparture/>Descubra</Link>
+    </div>
+  )
+}
