d8550a9f63b7d9f0ee37a6317f1f6fde151c112a
BryanVincent <bryanvincentrferreira@gmail.com>
2024-05-19 22:41:28 -0300

    feat(back)= Refatorando tabela de logs do banco de dados

--- database/db_trekio.sql
+++ database/db_trekio.sql
@@ -32,7 +32,9 @@ create table tb_logs(
 	id int primary key auto_increment,
 	perguntaChatGPT varchar(255),
     respostaChatGPT varchar(255),
-    logsDeConsultas varchar(255) not null
+    logsDeConsultas varchar(255) not null,
+	logsAcessoData varchar(255) not null,
+	logsAcessoHora varchar(255) not null
 );
 
 select * from tb_logs;
