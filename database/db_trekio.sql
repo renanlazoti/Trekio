0800b81a1c27dd96ef0f437e9c9de3920bcb9289
BryanVincent <bryanvincentrferreira@gmail.com>
2024-05-22 17:00:37 -0300

    feat(back): refatorando script sql

--- database/db_trekio.sql
+++ database/db_trekio.sql
@@ -1,30 +1,4 @@
 create database db_trekio;
-use db_trekio;
-
-create table tb_logs(
-	id int primary key auto_increment,
-    perguntaChatGPT varchar(255) not null,
-    respostaChatGPT varchar(255) not null
-);
-
-# ATUALIZAÇÃO DO BANCO DE DADOS
-
-select * from tb_logs;
-
-create database db_trekio;
-use db_trekio;
-
-create table tb_logs(
-	id int primary key auto_increment,
-	logsDeConsultas varchar(255) not null
-);
-
-select * from tb_logs;
-
-
-# ATUALIZAÇÃO FINAL BD_Trekio
-
-create database db_trekio;
 
 use db_trekio;
 
