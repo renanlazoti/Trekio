a23d944c44ecd66f33ac2c41f36f86aa04f54423
Renan <renanlazoti@gmail.com>
2024-05-24 20:05:06 -0300

    styles(front): cria nova estilizacao para o card

--- front/src/components/card/Card.js
+++ front/src/components/card/Card.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './card.css'
 
-export const Card = ({ text, img }) => {
+export const Card = ({ icon: Icon, text }) => {
   return (
     <div className='app__card'>
         <div className='app__card-border'>
-            <img src={ img } />
+            <Icon className="app__card-icon" />
         </div>
         <p className='app__card-text'>{ text }</p>
     </div>
